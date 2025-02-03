52a36654e76e9d5e3ed4284cef35002015ce2707
AlessandroGuadagnuolo <alexguadagnuolo@gmail.com>
2025-02-03 15:35:01 +0100

    Aggiunte diverse classi

--- src/Studente.java
+++ src/Studente.java
@@ -1,10 +1,19 @@
-public class Studente extends Utente {
-    private String matricola;
+import java.util.Date;
+import java.util.List;
 
+public class Studente extends Utente {
+    private String matricola; // chiave primaria
+    private Date dataNascita;
+    private String residenza;
+    private List<Esame> esami; // lista degli esami del corso di appartenenza (per i voti)
+    private Corso pianoDiStudi; // indica il mio corso di laurea scelto
+    private List<Esame> esamiSuperati;
+    private List<Esame> esamiSostenuti; // in fase di valutazione
+    private List<Esame> testCompletati;
+    private boolean tasse;
 
     public Studente(String nome, String cognome, String matricola) {
         super(nome, cognome);
         this.matricola = matricola;
     }
-
 }
