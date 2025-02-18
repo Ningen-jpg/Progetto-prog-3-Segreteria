c5313a79c55f63bada6f2eabbfb67383804123e4
AlessandroGuadagnuolo <alexguadagnuolo@gmail.com>
2025-02-18 16:21:42 +0100

    commit 18 febbraio

--- src/Studente.java
+++ src/Studente.java
@@ -1,20 +1,89 @@
 import java.util.Date;
 import java.util.List;
 
-//stiamo usando il factory method pattern
 public class Studente extends Utente {
     private String matricola; // chiave primaria
     private Date dataNascita;
     private String residenza;
-    private List<Esame> esami; // lista degli esami del corso di appartenenza (per i voti)
-    private CorsoDiLaurea pianoDiStudi; // indica il mio corso di laurea scelto
+    private List<Esame> esami;
+    private CorsoDiLaurea pianoDiStudi;
     private List<Esame> esamiSuperati;
-    private List<Esame> esamiSostenuti; // in fase di valutazione
+    private List<Esame> esamiSostenuti;
     private List<Esame> testCompletati;
     private boolean tasse;
 
     public Studente(String nome, String cognome, String matricola) {
         super(nome, cognome);
         this.matricola = matricola;
+    }
+
+    // Getters e setters per tutti gli attributi
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public Date getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(Date dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public String getResidenza() {
+        return residenza;
+    }
+
+    public void setResidenza(String residenza) {
+        this.residenza = residenza;
+    }
+
+    public List<Esame> getEsami() {
+        return esami;
+    }
+
+    public void setEsami(List<Esame> esami) {
+        this.esami = esami;
+    }
+
+    public CorsoDiLaurea getPianoDiStudi() {
+        return pianoDiStudi;
+    }
+
+    public void setPianoDiStudi(CorsoDiLaurea pianoDiStudi) {
+        this.pianoDiStudi = pianoDiStudi;
+    }
+
+    public List<Esame> getEsamiSuperati() {
+        return esamiSuperati;
+    }
+
+    public void setEsamiSuperati(List<Esame> esamiSuperati) {
+        this.esamiSuperati = esamiSuperati;
+    }
+
+    public List<Esame> getEsamiSostenuti() {
+        return esamiSostenuti;
+    }
+
+    public void setEsamiSostenuti(List<Esame> esamiSostenuti) {
+        this.esamiSostenuti = esamiSostenuti;
+    }
+
+    public List<Esame> getTestCompletati() {
+        return testCompletati;
+    }
+
+    public void setTestCompletati(List<Esame> testCompletati) {
+        this.testCompletati = testCompletati;
+    }
+
+    public boolean isTasse() {
+        return tasse;
+    }
+
+    public void setTasse(boolean tasse) {
+        this.tasse = tasse;
     }
 }
--- src/Utente.java
+++ src/Utente.java
@@ -3,14 +3,14 @@ public abstract class Utente {
     private String nome;
     private String cognome;
 
-    //factory method pattern
+    // factory method pattern
     public Utente(String id, String nome, String cognome) {
         this.id = id;
         this.nome = nome;
         this.cognome = cognome;
     }
 
-    //secondo costruttore per la segreteria
+    // secondo costruttore per la segreteria
     public Utente(String nome, String cognome) {
     }
 }
