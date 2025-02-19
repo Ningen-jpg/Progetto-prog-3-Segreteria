215bc1820212e5b6b9967f5911fd86f58cb0704b
AlessandroGuadagnuolo <alexguadagnuolo@gmail.com>
2025-02-19 17:35:33 +0100

    aggiunta di nuovo classe database 19 febbraio

---
+++ src/Database.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+    public static void main(String[] args) throws ClassNotFoundException {
+        try (final Connection connection =
+                     DriverManager.getConnection("jdbc:postgresql://programmazione3-programmazione3.j.aivencloud.com:19840/defaultdb?ssl=require&user=avnadmin&password=AVNS_Y5gjymttI8vcX96hEei");
+             final Statement statement = connection.createStatement();
+             final ResultSet resultSet = statement.executeQuery("SELECT version()")) {
+
+            while (resultSet.next()) {
+                System.out.println("Version: " + resultSet.getString("version"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
+    }
+}
